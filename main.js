aa1435e39963046f0fb2e136fa2254c0b99e9232
GergesBadr <110337209+GergesBadr@users.noreply.github.com>
2023-05-27 21:16:49 +0300

    added project files

---
+++ main.js
@@ -0,0 +1,36 @@
+const newsletter = document.querySelector(".newsletter"),
+  successPopup = document.querySelector(".success-popup"),
+  inputHolder = document.querySelector(".input-holder"),
+  input = document.querySelector(".email-input"),
+  submitBtn = document.getElementById("submit-btn"),
+  dismissMsg = document.getElementById("dismiss-msg");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (isEmail(input.value)) {
+    showSuccess();
+  } else {
+    inValidEmail();
+  }
+});
+dismissMsg.addEventListener("click", () => {
+  window.location.reload();
+});
+
+function showSuccess() {
+  newsletter.classList.add("hide");
+  successPopup.classList.add("show");
+}
+
+function isEmail(emailAdress) {
+  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailAdress.match(regex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function inValidEmail() {
+  inputHolder.classList.add("invalid");
+}
